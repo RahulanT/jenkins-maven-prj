51be2ce837eae6ed08c8c79362b717c93a97bfd0
Rahulan Thevkanthan <rahulan.thevakanthan@fdmgroup.com>
2024-04-09 09:38:22 -0400

    first commit

---
+++ jenkinsTestPrj/src/main/java/com/rwavbEnt/firstPkg/FirstHelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.com.rwavbEnt.firstPkg;
+
+public class FirstHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello ! This is the Jenkins Test Project.");
+	}
+
+}
---
+++ jenkinsTestPrj/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rwavb
+ *
+ */
+module jenkinsTestPrj {
+}
